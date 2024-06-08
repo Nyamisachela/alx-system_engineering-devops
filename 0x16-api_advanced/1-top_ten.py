0a212a63df0764253b44893d747081bdd1b97ba2
Nyamisachela <nyamisachelagat@gmail.com>
2024-06-08 06:07:55 -0700

    apis

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 """
 Function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit.
@@ -22,6 +22,21 @@ def top_ten(subreddit):
         for get_data in req.json().get("data").get("children"):
             dat = get_data.get("data")
             title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+
+
+        print(None)#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
